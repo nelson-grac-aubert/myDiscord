d61276ea24b24882b21b9774eda1af540b0a4803
Daroueche MARI <daroueche.mari@laplateforme.io>
2026-06-18 15:26:02 +0200

    design inscription connexion debut

---
+++ myDiscord/client/controller/auth_controller.c
@@ -0,0 +1,13 @@
+#include "auth_controller.h"
+#include "../model/user.h"
+#include <string.h>
+
+int auth_controller_login(const char *email, const char *password, User **out_user) {
+    // TEMPORAIRE : En attendant que le pote du réseau connecte le vrai socket,
+    // on simule une réponse positive si l'email contient "@"
+    if (strchr(email, '@') != NULL) {
+        // *out_user = user_create("UtilisateurTest", email, "hash", 1);
+        return 1; // Succès de la connexion
+    }
+    return 0; // Échec
+}
---
+++ myDiscord/main.c
@@ -0,0 +1,11 @@
+#include "client/view/ui_login.h"
+
+int main(int argc, char **argv) {
+    (void)argc;
+    (void)argv;
+
+    // Lance l'interface en mode SDL2
+    ui_login_init_and_run();
+
+    return 0;
+}
