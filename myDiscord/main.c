b3112da906e62edd89a5308715c095989dd5135a
nelson-grac-aubert <nelson.grac-aubert@laplateforme.io>
2026-06-22 10:50:25 +0200

    refactor(post-merge) : clean file architecture on first big project merge

--- myDiscord/main.c
+++ myDiscord/main.c
@@ -1,5 +1,5 @@
 #include "client/view/include/ui_welcome.h"
-#include "client/view/include/ui_chat.h" // ➡️ AJOUT : Pour pouvoir appeler l'interface du chat
+#include "client/view/include/ui_chat.h"
 #include <SDL2/SDL.h>
 #include <SDL2/SDL_ttf.h>
 
@@ -7,7 +7,7 @@ int main(int argc, char **argv) {
     (void)argc;
     (void)argv;
 
-    // 1. Initialisation globale (Une seule fois au démarrage de l'application)
+    // Global init
     if (SDL_Init(SDL_INIT_VIDEO) < 0) {
         return 1;
     }
@@ -16,16 +16,15 @@ int main(int argc, char **argv) {
         return 1;
     }
 
-    // 2. Lance l'interface d'accueil et récupère le résultat de la connexion
-    // welcome_ui_init_and_run renvoie désormais 1 si connecté, 0 si fermé avec la croix
+    // Start GUI, return 1 if connection successful, 0 when closed with window red cross
     int user_logged_in = welcome_ui_init_and_run();
 
-    // 3. Si l'utilisateur s'est connecté avec succès, on bascule sur la fenêtre de Chat !
+    // If user is successfully logged in, go to chat windows
     if (user_logged_in) {
         chat_ui_init_and_run();
     }
 
-    // 4. Fermeture propre et définitive de la SDL et TTF à la toute fin du programme
+    // Close SDL and TTF properly on program termination
     TTF_Quit();
     SDL_Quit();
 
